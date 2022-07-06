a2adb0750c7d761d732a078a7828266455176e13
isaac G. S. Silva <isaacgabrielssz@gmail.com>
2022-07-06 14:11:03 -0300

    v. 1.5.2

--- abra.js
+++ abra.js
@@ -1,7 +1,7 @@
 function sort(){
   var numerodDaSort = Math.floor(Math.random() * 200)
 
-  if(numerodDaSort >= 180){
+  if(numerodDaSort >= 190){
     document.getElementById("abra").style.display = "block"
     document.getElementById("fotoDePerfil").style.display = "none"
     document.getElementById("perfil").style.background = "none"
